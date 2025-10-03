3e0aee813cafe4f581348120b2f6f8700aedc44d
Kassiano <109632452+kassianoalves@users.noreply.github.com>
2025-10-03 20:43:36 -0300

    Update While.js
    Fiz um calculadora que após testar as operações, pergunta ao usuário se tem desejo de continuar ou encerrar.

--- aula5/while1.js
+++ aula5/while1.js
@@ -1,13 +1,28 @@
 /* Ler 2 números até o usuário informar que deseja sair do programa.
 */
 
-let continua = ['s', 'sim'];
+let continua = 's';
 let num1, num2;
 let operacao;
 
-while (continua == 's', 'sim') {
+while (continua == 's' || continua == 'sim') {
     num1 = parseInt(prompt('Numero 1: '));
     num2 = parseInt(prompt('Numero 2: '));
     operacao = prompt('operacao <+,-,*,/>');
-    continua;
+
+    if (operacao =='+') {
+        alert('Resultado '+ (num1 + num2));}
+        else if (operacao == '-'){
+            alert('Resultado '+ (num1 - num2));}
+        
+        else if (operacao == '*'){
+            alert('Resultado '+ (num1 * num2));}
+        
+        else if (operacao == '/'){
+            alert('Resultado '+ (num1 / num2));}
+        
+        else {alert('Operacao inválida')}
+    continua = prompt('Deseja permanecer? <s/n>');
+
 }
+alert('Obrigado, thcau!');
