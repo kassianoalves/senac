1aa25d06311632efcd075004ea5ac048ecc66b49
kassiano <kassianoalvessilva@gmail.com>
2025-12-05 19:03:47 -0300

    Update exe2.py

--- Python/aula6/exe2.py
+++ Python/aula6/exe2.py
@@ -1,6 +1,6 @@
 
 class Produto:
-    def __init__(self, nome, preco, estoque):
+    def __init__(self, nome:str, preco:float, estoque:int):
         self.nome = nome
         self.preco = preco
         self.estoque = estoque
